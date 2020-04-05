281c71751d1be8d5b89168fd00c3c0f2defa9693
Tin Ho Trong <hotinbk@gmail.com>
2020-04-05 13:19:07 +0700

    Updates

---
+++ precache-manifest.d36039f609d760dc7be84fe5466689a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ada6bf4e6c15cdafeba1a3bc8babaac",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "0886d899c399d933621f",
+    "url": "/rolodex/static/css/main.917eb62d.chunk.css"
+  },
+  {
+    "revision": "69e7b74fa2c2570ab393",
+    "url": "/rolodex/static/js/2.b24caf38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rolodex/static/js/2.b24caf38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0886d899c399d933621f",
+    "url": "/rolodex/static/js/main.4dd2dbfc.chunk.js"
+  },
+  {
+    "revision": "243e809145dba5b8da2f",
+    "url": "/rolodex/static/js/runtime-main.e4e9e6c3.js"
+  }
+]);
